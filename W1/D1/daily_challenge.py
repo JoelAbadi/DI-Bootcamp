a073d86a781be7a59bdcbb08856a0be35bbb3d81
JoelAbadi <joelabadi23@gmail.com>
2025-05-11 16:25:20 +0300

    daily challnege #1

---
+++ W1/D1/daily_challenge.py
@@ -0,0 +1,27 @@
+#Daily Challenge
+
+
+
+# user_characters = 10
+
+# user_input = (input('how many strings do you have?'))
+
+# print(len(user_input))
+
+# if len(user_input) < user_characters:
+#     print('String not long enough')
+
+# elif len(user_input) > user_characters:
+#     print('String too long')
+
+# else :
+#     print('perfect')
+
+# string = 'perfect'
+# print(string[0])
+# print(string[-1])
+
+# text= 'perfect'
+
+# for i in range(1,len(text)+1):
+#     print (text[:i])
--- W1/D1/exercise.py
+++ W1/D1/exercise.py
@@ -57,30 +57,3 @@
 # if(a) > user_height:
 #     print('You need to grow more to be able to ride')
 
-#Daily Challenge
-
-
-
-# user_characters = 10
-
-# user_input = (input('how many strings do you have?'))
-
-# print(len(user_input))
-
-# if len(user_input) < user_characters:
-#     print('String not long enough')
-
-# elif len(user_input) > user_characters:
-#     print('String too long')
-
-# else :
-#     print('perfect')
-
-# string = 'perfect'
-# print(string[0])
-# print(string[-1])
-
-# text= 'perfect'
-
-# for i in range(1,len(text)+1):
-#     print (text[:i])

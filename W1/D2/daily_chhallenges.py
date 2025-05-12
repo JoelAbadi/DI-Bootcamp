0e931b3275b65f019cf5828613a638a20d79fa79
JoelAbadi <joelabadi23@gmail.com>
2025-05-12 19:37:47 +0300

    correction of daily challenge

--- W1/D2/daily_chhallenges.py
+++ W1/D2/daily_chhallenges.py
@@ -1,25 +1,48 @@
-#Daily Challenges
-#Part 1
+# Challenge 1: Multiples of a Number
 
-# number= int(input("Enter a number:'))
-# length = int(input('Enter how many multiples you want:'))
+# This program asks the user for a number and a length,
+# then generates a list of that many multiples of the number.
 
-# multiples={}
+# Ask the user for a number to multiply
+try:
+    number_to_multiply = int(input("Enter a number to multiply: "))
+    desired_length = int(input("Enter the number of multiples to generate: "))
+except ValueError:
+    print("Invalid input. Please enter whole numbers only.")
+    exit()
 
-# for i in range(1, length+1):
-# multiples.append(number*1)
+# Create an empty list to store the multiples
+multiples_list = []
 
-# Print (multiples)
+# Generate the multiples using a loop
+for i in range(1, desired_length + 1):
+    multiples_list.append(number_to_multiply * i)
+
+# Print the list of multiples
+print("List of multiples:", multiples_list)
 
 
+# Challenge 2: Remove Consecutive Duplicate Letters
 
-# #Part 2
+# This program removes consecutive duplicate characters from a string,
+# so "ppoeemm" becomes "poem".
 
-# user_input = input('enter a word:')
+# Ask the user for a string
+input_string = input("Enter a word or sentence: ")
 
-# result=""
-#  for i in range(len(user_input)):
-#  if i ==0 or user_input[i] != user_input[i-1]:
-#  result += user_input[i]
+# Check if the input string is not empty
+if input_string != "":
+    # Initialize the result with the first character
+    modified_string = input_string[0]
 
-# print('modified string:', results)
+    # Loop through the remaining characters
+    for char in input_string[1:]:
+        # Add the character only if it's not the same as the last one added
+        if char != modified_string[-1]:
+            modified_string += char
+
+    # Print the result
+    print("Modified string without consecutive duplicates:", modified_string)
+else:
+    print("You entered an empty string.")
+
